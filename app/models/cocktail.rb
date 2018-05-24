3b6eea9522e0d68470a442ec726ef69d80a4ce0d
David Zwagemaker <david@zwagemaker.nl>
2018-05-24 10:06:01 +0100

    Added all validations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,9 @@
 class Cocktail < ApplicationRecord
-  has_many :doses
+  has_many :doses, dependent: :delete_all
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
+  # before_destroy :clear_doses
+
+  private
 end
+
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,5 +4,6 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail, presence: true
   validates :ingredient, presence: true
+  validates_uniqueness_of :ingredient, scope: :cocktail
 
 end
