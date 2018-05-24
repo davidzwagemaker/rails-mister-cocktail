f51a0ef6601edf8b53ce55834af20338be3b351a
David Zwagemaker <david@zwagemaker.nl>
2018-05-24 18:02:31 +0100

    Added working ingredient controller and pages. Not yet protected from deleting ingredients that are used in doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'ingredients/show'
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
 
   resources :doses, only: [:destroy]
-  resources :ingredients, only: [:index, :show, :new, :create, :destroy]
+  resources :ingredients, only: [:index, :new, :create, :destroy]
 
 end
