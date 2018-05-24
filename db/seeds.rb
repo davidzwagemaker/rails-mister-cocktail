bcb1726c82b11366d596bf82c6a538b1b21b746c
David Zwagemaker <david@zwagemaker.nl>
2018-05-24 10:30:05 +0100

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Adding Ingredients"
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "fresh lemon juice")
+Ingredient.create(name: "syrup")
+Ingredient.create(name: "crushed ice")
+
+p "Adding Cocktails"
+Cocktail.create(name: "Whiskey Sour")
+
+p "Adding Doses"
+Dose.create(description: "2 doses", ingredient: Ingredient.find(1), cocktail: Cocktail.find(1))
+Dose.create(description: "1 doses", ingredient: Ingredient.find(2), cocktail: Cocktail.find(1))
+Dose.create(description: "1 doses", ingredient: Ingredient.find(3), cocktail: Cocktail.find(1))
+Dose.create(description: "Add to flavor", ingredient: Ingredient.find(4), cocktail: Cocktail.find(1))
+
